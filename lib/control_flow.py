ba791fab20824374f7eac0c7acc34b55be91c78c
danwycliff <danwycliff6769@gmail.com>
2023-09-12 22:02:11 +0300

    codes

--- lib/control_flow.py
+++ lib/control_flow.py
@@ -10,38 +10,38 @@ def admin_login(username, password):
 
 def hows_the_weather(temperature):
     # your code here
-    if temperature < 40:
-        return "It's brisk out there!"
-    elif (temperature >= 40 or temperature <=65):
-        return "It's a little chilly out there!"
-    elif temperature > 85:
-        return ")It's too dang hot out there!"
+    if (temperature < 40):
+        return "It\'s brisk out there!"
+    elif ((temperature > 40) and (temperature < 65)):
+        return "It\'s a little chilly out there!"
+    elif (temperature > 85):
+        return "It\'s too dang hot out there!"
     else :
-        return "It's perfect out there!"
+        return "It\'s perfect out there!"
 
 
 def fizzbuzz(num):
     # your code here
-    if num % 3 == 0:
-        return "Fizz"
-    elif num % 5 == 0:
-        return "Buzz"
-    elif num % 5 ==0 and num % 3 ==0:
+    if ((num%5 == 0) and (num%3 == 0)):
         return "FizzBuzz"
-    else :
+    elif (num%3 == 0):
+        return "Fizz"
+    elif (num%5 == 0):
+        return "Buzz"
+    else:
         return num
     
 
 
 def calculator(operation, num1, num2):
-    if operation == "+":
+    if (operation == "+"):
         return num1 + num2
-    elif operation == "-":
+    elif (operation == "-"):
         return num1 - num2
-    elif operation == "*":
+    elif (operation == "*"):
         return num1 * num2
-    elif operation == "/":
-        if num2 != 0:
+    elif (operation == "/"):
+        if (num2 != 0):
             return num1 / num2
         else:
             print("Division by zero is not allowed!")
